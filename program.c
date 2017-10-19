c0ff161dda2ec9cf1c2b468c0b665031a7fd306a
pskowronwspol <piotr.skowron101@onet.pl>
2017-10-19 16:04:49 +0200

    pierwszy program zakonczony

---
+++ program.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*  y = f(x)		*/
+/* 	main() => 0	*/
+
+
+int main() /* funkcja glowna programu, zwraca wartosc calkowita */
+{/* miedzy klamrami jest cialo funkcji */
+
+	puts("Pierwszy program"); /* funkcja z biblioteki stdio */
+
+	return 0; /*main zwraca wartosc 0*/
+
+}
+
+
+
+
